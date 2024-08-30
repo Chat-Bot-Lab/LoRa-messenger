5e3058d2a688ee63ddfe99d7295daf7d207ee802
vladimir <prstud@yandex.ru>
2024-08-30 12:30:15 +0300

    add encrypt and decrypt methods

---
+++ utils/encryption.py
@@ -0,0 +1,40 @@
+from environs import Env
+
+env = Env()
+
+symbols = env("SYMBOLS")
+offset_value = env("OFFSET_VALUE")
+
+
+def encrypt(message: str) -> str:
+    """
+    Шифрует сообщение, сдвигая каждый символ в сообщении на заданное значение смещения.
+    """
+    encr_message = ""
+    for symb in message:
+        index = symbols.find(symb)
+        new_index = index + offset_value
+        if new_index > len(symbols) - 1:
+            new_index = new_index - (len(symbols) - 1)
+        if symb in symbols:
+            encr_message += symbols[new_index]
+        else:
+            encr_message += symb
+    return encr_message
+
+
+def decrypt(message: str) -> str:
+    """
+    Расшифровывает сообщение, которое ранее было зашифровано с помощью функции encrypt().
+    """
+    decr_message = ""
+    for symb in message:
+        place = symbols.find(symb)  # Алгоритм для шифрования сообщения на русском
+        new_place = place - offset_value
+        if new_place < 0:
+            new_place -= 1
+        if symb in symbols:
+            decr_message += symbols[new_place]
+        else:
+            decr_message += symb
+    return decr_message
